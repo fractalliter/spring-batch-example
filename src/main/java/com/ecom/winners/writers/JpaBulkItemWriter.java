bf4d1d5bfbe39cc748981d9a7334de24fa6f985c
elias <eliass.rahmani@gmail.com>
2023-04-22 21:56:00 +0200

    add JPA bulk item writer

---
+++ src/main/java/com/ecom/winners/writers/JpaBulkItemWriter.java
@@ -0,0 +1,68 @@
+package com.ecom.winners.writers;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.orm.jpa.EntityManagerFactoryUtils;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+public class JpaBulkItemWriter<T> implements ItemWriter<List<T>>, InitializingBean {
+    protected static final Log logger = LogFactory.getLog(JpaBulkItemWriter.class);
+    private EntityManagerFactory entityManagerFactory;
+    private boolean usePersist = false;
+
+    public JpaBulkItemWriter() {
+    }
+
+    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public void setUsePersist(boolean usePersist) {
+        this.usePersist = usePersist;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        Assert.state(this.entityManagerFactory != null, "An EntityManagerFactory is required");
+    }
+
+    public void write(Chunk<? extends List<T>> listOfItems) {
+        EntityManager entityManager = EntityManagerFactoryUtils.getTransactionalEntityManager(this.entityManagerFactory);
+        if (entityManager == null) {
+            throw new DataAccessResourceFailureException("Unable to obtain a transactional EntityManager");
+        } else {
+            this.doWrite(entityManager, listOfItems);
+            entityManager.flush();
+        }
+    }
+
+    protected void doWrite(EntityManager entityManager, Chunk<? extends List<T>> listOfItems) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Writing to JPA with " + listOfItems.size() + " items.");
+        }
+
+        if (!listOfItems.isEmpty()) {
+            long addedToContextCount = 0L;
+
+            for (List<T> item : listOfItems) {
+                for (T i : item)
+                    entityManager.persist(i);
+
+                ++addedToContextCount;
+            }
+
+            if (logger.isDebugEnabled()) {
+                logger.debug(addedToContextCount + " entities " + (this.usePersist ? " persisted." : "merged."));
+                logger.debug((long) listOfItems.size() - addedToContextCount + " entities found in persistence context.");
+            }
+        }
+
+    }
+}
