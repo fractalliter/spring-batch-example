4e793d6c8c49353ed3d2deed184972c74e2e5f8e
elias <eliass.rahmani@gmail.com>
2023-04-22 21:56:32 +0200

    add kafka configurations

---
+++ src/main/java/com/ecom/winners/configurations/KafkaConfiguration.java
@@ -0,0 +1,21 @@
+package com.ecom.winners.configurations;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Value("${spring.kafka.template.default-topic}")
+    private String weeklyWinnerTopic;
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(weeklyWinnerTopic)
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+}
