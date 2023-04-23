1c7dd016535ad52c4a6f130e177b3228f56f8adc
elias <eliass.rahmani@gmail.com>
2023-04-23 21:02:46 +0200

    prepare context for test

--- src/test/java/com/ecom/winners/WinnersApplicationTests.java
+++ src/test/java/com/ecom/winners/WinnersApplicationTests.java
@@ -1,15 +1,27 @@
 package com.ecom.winners;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBatchTest
 @SpringBootTest
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 class WinnersApplicationTests {
 
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
     @Test
+    @DisplayName("application context loads")
     void contextLoads() {
+        assertNotNull(jobLauncherTestUtils);
     }
 
 }
